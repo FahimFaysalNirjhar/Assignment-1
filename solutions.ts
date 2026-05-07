5baeeaa4f8ca79ff6ba56724205618a70def38ab
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-05-07 20:25:39 +0600

    remove all the console.log from solutions.ts

--- solutions.ts
+++ solutions.ts
@@ -4,16 +4,11 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
 };
 
 const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log(result);
 
 // Task 2
 const reverseString = (str: string): string => {
   return str.split("").reverse().join("");
 };
-
-console.log(reverseString("typescript"));
-
-console.log(reverseString("type"));
 
 //Task 3
 type StringOrNumber = string | number;
@@ -23,16 +18,12 @@ function checkType(value: StringOrNumber): string {
   return "Number";
 }
 
-console.log(checkType("Hello"));
-console.log(checkType(42));
-
 //Task 4
 const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
   return obj[key];
 };
 
 const user = { id: 1, name: "John Doe", age: 21 };
-console.log(getProperty(user, "name"));
 
 //Task 5
 interface Book {
@@ -53,7 +44,6 @@ const myBook = {
   author: "Jane Doe",
   publishedYear: 2024,
 };
-console.log(toggleReadStatus(myBook));
 
 //Task 6
 class Person {
@@ -81,11 +71,7 @@ class Student extends Person {
 
 const student = new Student("Alice", 20, "A");
 
-console.log(student.getDetails());
-
 //Task 7
 function getIntersection(array1: number[], array2: number[]): number[] {
   return array1.filter((num) => array2.includes(num));
 }
-
-console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
